24162a4ef18f3299dbd8895a71f3ea3e868bb25e
Swapnil Basate <basateswapnil@gmail.com>
2025-01-31 12:15:05 +0530

    404 error fixed

---
+++ script.js
@@ -0,0 +1,22 @@
+gsap.from('.header',{
+    opacity:0,
+    y:-30,
+    delay:0.5,
+    duration:0.5,
+})
+gsap.from('.header h2',{
+    y:-45,
+    delay:1,
+    duration:0.5,
+})
+gsap.from('.header .links a',{
+    y:-45,
+    delay:1.5,
+    duration:0.5,
+    stagger:0.6
+})
+gsap.from('.header .links button',{
+    y:-45,
+    delay:3.9,
+    duration:0.5,
+})
